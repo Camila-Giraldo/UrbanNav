1e8d9c07363061b0f52b032b5190a7d56c8177ac
Camila-Giraldo <gc.camila28@gmail.com>
2023-09-20 11:08:00 -0500

    Creación de los modelos del microservicio de seguridad

---
+++ src/models/login.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Login extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  code2Fa: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  codeStatus: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  token: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  tokenStatus: boolean;
+
+  constructor(data?: Partial<Login>) {
+    super(data);
+  }
+}
+
+export interface LoginRelations {
+  // describe navigational properties here
+}
+
+export type LoginWithRelations = Login & LoginRelations;
